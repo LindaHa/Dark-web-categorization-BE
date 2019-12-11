17060456a3f486fbf0a0f773305578becf6fbd24
lindaha <hanslik.linda@gmail.com>
2019-12-11 18:35:37 +0100

    Fix links between categories

--- api/utils/graph_helpers/group_by_helpers.py
+++ api/utils/graph_helpers/group_by_helpers.py
@@ -1,7 +1,7 @@
 from typing import Dict, List
 from enum import Enum
 from api.categorization.labels import labels_index
-from api.models import Page, Group, Category, Link
+from api.models import Page, Group, Link
 from api.utils.graph_helpers.graph_helpers import get_linked_groups_from_ids
 
 
@@ -18,7 +18,7 @@ def get_partition_by_category(pages: Dict[str, Page]) -> Dict[str, int]:
     :rtype: Dict[str, int]
     """
     partition = {}
-    reversed_labels_index = {label: key for key, label in labels_index.items()}
+    reversed_labels_index: Dict[str, int] = {label: key for key, label in labels_index.items()}
 
     for url, page in pages.items():
         p_category = page.categories[0].name
@@ -51,6 +51,19 @@ def divide_pages_by_category(pages: Dict[str, Page]) -> List[Group]:
     """
     partition = get_partition_by_category(pages)
     groups = get_linked_groups_from_ids(pages, partition)
-    renamed_groups = name_by_category(groups)
+    converted_linked_groups = convert_link_aliases_to_keys(groups, partition)
+    renamed_groups = name_by_category(converted_linked_groups)
 
     return renamed_groups
+
+
+def convert_link_aliases_to_keys(groups: List[Group]) -> List[Group]:
+    for group in groups:
+        converted_links = set()
+        for link in group.links:
+            reversed_link = labels_index[int(link.link)]
+            converted_link = Link(link=reversed_link)
+            converted_links.add(converted_link)
+        group.links = list(converted_links)
+
+    return groups
