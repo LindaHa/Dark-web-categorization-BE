4a3d4ce0b7c6bcc7c34d0bfcd5b9dce83b0bf5f5
lindaha <hanslik.linda@gmail.com>
2020-02-26 16:25:53 +0100

    Do not display links to themselves

--- api/utils/graph_helpers/graph_helpers.py
+++ api/utils/graph_helpers/graph_helpers.py
@@ -74,7 +74,7 @@ def get_linked_groups_from_ids(
             for link in links:
                 link_to_group = partition.get(link)
                 whole_id = str(parent_key_prefix) + str(link_to_group)
-                if link_to_group is not None:
+                if link_to_group is not None and link_to_group != group_id:
                     if str(link_to_group) not in group_links:
                         new_link = Link(link=whole_id, occurrences=1)
                         group_links[whole_id] = new_link
